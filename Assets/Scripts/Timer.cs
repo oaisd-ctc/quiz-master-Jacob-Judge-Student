be083e0ceb2c8c3f1249c7b64799ec5e50edadd0
Jacob-Judge-Student <jajuf70@student.oaisd.org>
2023-03-23 10:32:52 -0400

    quiz master
    asdasdasdad

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    public bool isAnsweringQuestion = false;
+    float timerValue;
+    
+     void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if(timerValue <= 0)
+        {
+            timerValue = timeToCompleteQuestion;
+        }
+        Debug.Log(timerValue);
+    }
+}
